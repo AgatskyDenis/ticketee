00d8b00589660df908489c8505ffb1d7902fd909
AgatskyDenis <agatsky_den@mail.ru>
2011-09-26 01:22:46 +0900

    Creation of projects feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ticketee::Application.routes.draw do
+  resources :projects
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +49,7 @@ Ticketee::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'projects#index'
 
   # See how all your routes lay out with "rake routes"
 
