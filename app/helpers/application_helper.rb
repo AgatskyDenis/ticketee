8c475274f635a98561cd15afd7f1e58d52e5ae0b
AgatskyDenis <agatsky_den@mail.ru>
2011-09-26 02:08:04 +0900

    Added title functionality for show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+def title(*parts)
+  unless parts.empty?
+    content_for :title do
+      (parts << "Ticketee").join(" - ") unless parts.empty?
+    end
+  end
+end
+
 end
