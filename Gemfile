4a1a08ea95740814bd4de3c41a3878c35284ec3f
AgatskyDenis <agatsky_den@mail.ru>
2011-10-02 17:28:10 +0900

    Add validation to ensure names are specified when creating projects

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'therubyracer'
 group :assets do
   gem 'sass-rails', "  ~> 3.1.0"
   gem 'coffee-rails', "~> 3.1.0"
+  gem 'dynamic_form'
   gem 'uglifier'
 end
 
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,13 +5,18 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
-  
-  def create
-    @project = Project.new(params[:project])
-    @project.save
+
+def create
+  @project = Project.new(params[:project])
+  if @project.save
     flash[:notice] = "Project has been created."
     redirect_to @project
+  else
+    flash[:alert] = "Project has not been created."
+    render :action => "new"
   end
+end
+
 
   def show
     @project = Project.find(params[:id])
