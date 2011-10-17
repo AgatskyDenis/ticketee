20ec828c991fa30028ea7a50649aefa2cf251e03
AgatskyDenis <agatsky_den@mail.ru>
2011-10-17 14:42:56 +0900

    Added admin namespaced users controller

--- config/routes.rb
+++ config/routes.rb
@@ -60,4 +60,9 @@ Ticketee::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+namespace :admin do
+  resources :users
+end
+
 end
---
+++ spec/support/seed_helpers.rb
@@ -0,0 +1,10 @@
+module SeedHelpers
+  def create_user!(attributes={})
+    user = Factory(:user, attributes)
+    user.confirm!
+    user
+  end
+end
+RSpec.configure do |config|
+  config.include SeedHelpers
+end
