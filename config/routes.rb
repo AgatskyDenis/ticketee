8450db5f6860b8d55896fe45d2c03f5507ac90d0
AgatskyDenis <agatsky_den@mail.ru>
2011-10-28 14:35:11 +0900

    Fix signing up feature,

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Ticketee::Application.routes.draw do
-  devise_for :users
+#  get "users/confirmation"
+
+  devise_for :users, :controllers => { :registrations => "registrations" }
+
+  get '/awaiting_confirmation',
+    :to => "users#confirmation",
+    :as => 'confirm_user'
+
 
   resources :projects do
     resources :tickets
--- features/signing_up.feature
+++ features/signing_up.feature
@@ -10,3 +10,4 @@ Scenario: Signing up
   And I fill in "Password confirmation" with "password"
   And I press "Sign up"
   Then I should see "You have signed up successfully."
+  Then I should see "Please confirm your account before signing in."
