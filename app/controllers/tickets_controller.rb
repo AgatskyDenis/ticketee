f53db6b7d991296012249607bc8e128cd1e4b1d2
AgatskyDenis <agatsky_den@mail.ru>
2011-11-07 12:19:28 +0800

    Restricting creating tickets to only users

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,58 +2,68 @@ class TicketsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :find_project
   before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
-
+  before_filter :authorize_create!, :only => [:new, :create]
+  
   def new
     @ticket = @project.tickets.build
   end
-
-  def show
-  end
-
-def create
-  #@ticket = @project.tickets.build(params[:ticket])
-  @ticket = @project.tickets.build(params[:ticket].merge!(:user => current_user))
+  
+  def create
+    @ticket = @project.tickets.build(params[:ticket].merge!(:user => current_user))
     if @ticket.save
-        flash[:notice] = "Ticket has been created."
-        redirect_to [@project, @ticket]
+      flash[:notice] = "Ticket has been created."
+      redirect_to [@project, @ticket]
     else
-        flash[:alert] = "Ticket has not been created."
-        render :action => "new"
+      flash[:alert] = "Ticket has not been created."
+      render :action => "new"
     end
-end
+  end
+  
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    if @ticket.update_attributes(params[:ticket])
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been updated."
+      render :action => "edit"
+    end
+  end
+  
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+    redirect_to @project
+  end
+    
+  
+  private
+    def find_project
+      @project = Project.for(current_user).find(params[:project_id])
+      rescue ActiveRecord::RecordNotFound
+        flash[:alert] = "The project you were looking for could not be found."
+        redirect_to root_path
+    end
+    
+    def find_ticket
+      @ticket = @project.tickets.find(params[:id])
+    end
 
-   def edit
-   end
-
-def update
-  if @ticket.update_attributes(params[:ticket])
-    flash[:notice] = "Ticket has been updated."
-    redirect_to [@project, @ticket]
-  else
-    flash[:alert] = "Ticket has not been updated."
-    render :action => "edit"
+def authorize_create!
+  if !current_user.admin? && cannot?("create tickets".to_sym, @project)
+    flash[:alert] = "You cannot create tickets on this project."
+    redirect_to @project
   end
 end
 
-def destroy
-  @ticket.destroy
-  flash[:notice] = "Ticket has been deleted."
-  redirect_to @project
-end
 
 end
-
-private
-  def find_project
-    @project = Project.for(current_user).find(params[:project_id])
-    rescue ActiveRecord::RecordNotFound
-    flash[:alert] = "The project you were looking for could not be found."
-    redirect_to root_path
-  end
-
-  def find_ticket
-    @ticket = @project.tickets.find(params[:id])
-  end
-
 
 
--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -12,4 +12,27 @@ describe TicketsController do
       flash[:alert].should eql("The project you were looking for could not be found.")
     end
   end
+
+context "with permission to view the project" do
+  before do
+    sign_in(:user, user)
+    Permission.create!(:user => user, :thing => project, :action => "view")
+  end
+  def cannot_create_tickets!
+    response.should redirect_to(project)
+    flash[:alert].should eql("You cannot create tickets on this project.")
+  end
+  it "cannot begin to create a ticket" do
+    get :new, :project_id => project.id
+    cannot_create_tickets!
+  end
+  it "cannot create a ticket without permission" do
+    post :create, :project_id => project.id
+    cannot_create_tickets!
+  end
 end
+
+
+end
+
+
