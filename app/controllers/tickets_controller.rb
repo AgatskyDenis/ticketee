0d0f4bfd57e687323da155810627cc11c0c49c83
AgatskyDenis <agatsky_den@mail.ru>
2011-11-10 10:28:17 +0800

    Restrict destroy action to only people with permission

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,6 +4,7 @@ class TicketsController < ApplicationController
   before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
   before_filter :authorize_create!, :only => [:new, :create]
   before_filter :authorize_update!, :only => [:edit, :update]
+  before_filter :authorize_delete!, :only =>  :destroy
   
   def new
     @ticket = @project.tickets.build
@@ -52,26 +53,35 @@ class TicketsController < ApplicationController
         flash[:alert] = "The project you were looking for could not be found."
         redirect_to root_path
     end
-    
+
     def find_ticket
       @ticket = @project.tickets.find(params[:id])
     end
 
-def authorize_create!
-  if !current_user.admin? && cannot?("create tickets".to_sym, @project)
-    flash[:alert] = "You cannot create tickets on this project."
-    redirect_to @project
-  end
+    def authorize_create!
+      if !current_user.admin? && cannot?("create tickets".to_sym, @project)
+        flash[:alert] = "You cannot create tickets on this project."
+        redirect_to @project
+      end
+    end
+
+    def authorize_update!
+      if !current_user.admin? && cannot?(:"edit tickets", @project)
+        flash[:alert] = "You cannot edit tickets on this project."
+        redirect_to @project
+      end
+    end
+
+    def authorize_delete!
+      if !current_user.admin? && cannot?(:"delete tickets", @project)
+        flash[:alert] = "You cannot delete tickets from this project."
+        redirect_to @project
+      end
+    end
+
 end
 
-def authorize_update!
-  if !current_user.admin? && cannot?(:"edit tickets", @project)
-    flash[:alert] = "You cannot edit tickets on this project."
-    redirect_to @project
-  end
-end
 
-end
 
 
 
