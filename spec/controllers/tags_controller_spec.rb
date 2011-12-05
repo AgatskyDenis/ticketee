d6d4d6590852238ad609fe9541aad6c353d18bdf
AgatskyDenis <agatsky_den@mail.ru>
2011-12-05 11:04:17 +0900

    Users may now search for tickets by state or tag

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 describe TagsController do
+=begin
+  describe "GET 'remove'" do
+    it "should be successful" do
+      get 'remove'
+      response.should be_success
+    end
+  end
+=end
 end
+
+
